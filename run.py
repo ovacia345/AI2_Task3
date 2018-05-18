ee79627280da1ba4c203ab4a6b2df85a1c170cdd
ovacia345 <nielsvann@hotmail.nl>
2018-05-18 14:23:24 +0200

    Test with earthquake.bif

--- run.py
+++ run.py
@@ -8,31 +8,39 @@ from read_bayesnet import BayesNet
 from variable_elim import *
 
 if __name__ == '__main__':
-    # the class BayesNet represents a Bayesian network from a .bif file
-    # in several variables
-    net = BayesNet('knowledgeClip.bif')
-    
-    # these are the variables that should be used for variable elimination
-    print 'values', net.values 
-    print 'probabilities', net.probabilities
-    print 'parents', net.parents
-    print 'nodes', net.nodes
+    # # the class BayesNet represents a Bayesian network from a .bif file
+    # # in several variables
+    # net = BayesNet('knowledgeClip.bif')
+    #
+    # # these are the variables that should be used for variable elimination
+    # print 'values', net.values
+    # print 'probabilities', net.probabilities
+    # print 'parents', net.parents
+    # print 'nodes', net.nodes
+    #
+    #
+    # # Make your variable elimination code in a seperate file: 'variable_elim'.
+    # # you can call this file as follows:
+    # ve = VariableElimination(net)
+    #
+    # # If variables are known beforehand, you can represent them in the following way:
+    # evidence = {}
+    #
+    # # determine you heuristics before you call the run function. This can be done in this file or in a seperate file
+    # # The heuristics either specifying the elimination ordering (list) or it is a function that determines the elimination ordering
+    # # given the network. An simple example is:
+    # query = ['E']
+    # elim_order = ['D', 'F', 'A', 'B', 'C']
+    #
+    # #call the elimination ordering function for example as follows:
+    # ve.run(query, evidence, elim_order)
 
-    
-    # Make your variable elimination code in a seperate file: 'variable_elim'. 
-    # you can call this file as follows:
+
+    net = BayesNet('earthquake.bif')
     ve = VariableElimination(net)
-
-    # If variables are known beforehand, you can represent them in the following way: 
-    evidence = {}
-
-    # determine you heuristics before you call the run function. This can be done in this file or in a seperate file
-    # The heuristics either specifying the elimination ordering (list) or it is a function that determines the elimination ordering
-    # given the network. An simple example is:
-    query = ['E']
-    elim_order = ['D', 'F', 'A', 'B', 'C']
-
-	#call the elimination ordering function for example as follows:   
+    evidence = {'Burglary': 'True'}
+    query = 'Alarm'
+    elim_order = ['Earthquake', 'JohnCalls', 'MaryCalls']
     ve.run(query, evidence, elim_order)
 
 
--- variable_elim.py
+++ variable_elim.py
@@ -12,10 +12,7 @@ class VariableElimination():
         self.network = network
         self.network.values = {node: sorted(self.network.values[node]) for node in self.network.values}
 
-        self.addition_steps =  0
-        self.multiplication_steps = 0
 
-    ## Finished
     def run(self, query, observed, elim_order):
         factors = np.array([])
         for node in self.network.nodes:
@@ -26,38 +23,18 @@ class VariableElimination():
         for node in elim_order:
             node_factors_indices = [i for i, factor in enumerate(factors) if node in factor.nodes]
 
-            node_factors = factors[node_factors_indices]
-            for i in xrange(node_factors.size - 1):
-                product = node_factors[i].times(node_factors[i + 1])
-                node_factors[i + 1] = product
+            product = Factor.product(factors[node_factors_indices])
 
-            marginalization = node_factors[-1].marginalize(node)
+            marginalization = product.marginalize(node)
 
             factors = np.append(factors, marginalization)
             factors = np.delete(factors, node_factors_indices)
 
-        print
-        print "Result:"
-        print factors[0].nodes
-        print factors[0].probs
 
-        """
-        Use the variable elimination algorithm to find out the probability
-        distribution of the query variable given the observed variables
+        result = Factor.product(factors)
 
-        Input:
-            query:      The query variable
-            observed:   A dictionary of the observed variables {variable: value}
-            elim_order: Either a list specifying the elimination ordering
-                        or a function that will determine an elimination ordering
-                        given the network during the run
+        return result.normalize()
 
-        Output: A variable holding the probability distribution
-                for the query variable
-
-        """
-
-    ## Finished
     def makeFactor(self, node, observed):
         nodes = np.array([node] + self.network.parents[node])
         probs = self.network.probabilities[node].values
@@ -66,7 +43,7 @@ class VariableElimination():
         if nodes_in_observed.size > 0:
             if nodes.size > nodes_in_observed.size:
                 factor = Factor(nodes, probs, self.network)
-                return factor.reduce(nodes_in_observed)
+                return factor.reduce(observed, nodes_in_observed)
             else:
                 return Factor()
         else:
@@ -84,7 +61,34 @@ class Factor():
 
         self.network = network
 
-    ## Finished
+
+    @staticmethod
+    def product(factors):
+        for i in xrange(factors.size - 1):
+            factors[1] = factors[0].times(factors[1])
+            factors = np.delete(factors, 0)
+        return factors[0]
+
+    @staticmethod
+    def get_probs(factor1, factor2, prob_column, nr_common_nodes, nodes):
+        values1 = factor1.probs[:, :-1]  # Includes common_nodes columns
+        values2 = factor2.probs[:, nr_common_nodes:-1]  # Excludes common_nodes columns
+
+        if values1[:, nr_common_nodes:].size != 0 and values2.size != 0:
+            nr_rows = np.prod([len(factor1.network.values[node] for node in nodes)])
+
+            probs = [np.repeat(values1, nr_rows / len(values1), axis=0)]
+            probs += [np.tile(values2.T, nr_rows / len(values2)).T]
+        elif values2.size != 0:
+            probs = [np.repeat(values1, len(values2) / len(values1), axis=0)]
+            probs += [values2]
+        else:
+            probs = [values1]
+        probs += [prob_column]
+
+        return np.hstack(probs)
+
+
     def times(self, factor):
         common_nodes = np.intersect1d(self.nodes, factor.nodes)
         nr_common_nodes = common_nodes.size
@@ -100,11 +104,10 @@ class Factor():
         products = [[products1[i, e] * products2[i] for e in xrange(len(products1[0]))] for i in xrange(len(products1))]
         products = np.vstack([product.reshape((-1, 1)) for value_products in products for product in value_products])
 
-        probs = get_probs(self, factor, products, nr_common_nodes, nodes)
+        probs = Factor.get_probs(self, factor, products, nr_common_nodes, nodes)
 
         return Factor(nodes, probs, self.network)
 
-    ## Finished
     def marginalize(self, node):
         self.sort([node])
         nr_values = len(self.network.values[node])
@@ -120,23 +123,28 @@ class Factor():
 
         return Factor(self.nodes[1:], probs, self.network)
 
-    def reduce(self, observed_nodes):
-        keys = observed_nodes.keys()
+    ## TODO
+    def reduce(self, observed, nodes_in_observed):
+        self.sort(nodes_in_observed)
 
-        self.sort(keys)
+        nodes_in_observed = [(node, observed[node]) for node in nodes_in_observed]
+        nodes_in_observed_values_data = [(self.network.values[node].index(value), len(self.network.values[node]))
+                                            for node, value in nodes_in_observed]
 
-        for key in keys:
-            values = np.array(self.network.values[key])
-            value_index = np.flatnonzero(values == observed_nodes[key])[0]
-            nr_equal_values_in_column = len(self.probs) / len(values)
-            bounds = [value_index * nr_equal_values_in_column, (value_index + 1) * nr_equal_values_in_column]
+        nr_rows = len(self.probs)
+        bounds = [0, nr_rows]
+        for index, nr_values in nodes_in_observed_values_data:
+            nr_equal_values_in_column = nr_rows / nr_values
+            bounds[0] += index * nr_equal_values_in_column
+            bounds[1] -= (nr_values - index - 1) * nr_equal_values_in_column
+            nr_rows /= nr_values
 
-            self.probs = self.probs[bounds[0] : bounds[1], 1:]
+        probs = self.probs[bounds[0] : bounds[1], len(nodes_in_observed):]
 
-        self.nodes = self.nodes[len(keys):]
-        self.nr_nodes -= len(keys)
+        nodes = self.nodes[len(nodes_in_observed):]
 
-    ## Finished
+        return Factor(nodes, probs, self.network)
+
     def sort(self, nodes):
         nodes_columns_indices = np.flatnonzero(np.in1d(self.nodes, nodes))
 
@@ -152,30 +160,16 @@ class Factor():
 
         self.probs = np.array(map(list, self.probs))
 
-    ## Finished
     def products_with_equal_common_nodes(self, nr_common_nodes_values_combinations):
         nr_equal_common_nodes_values_in_columns = len(self.probs) / nr_common_nodes_values_combinations
         bounds_list = [[i * nr_equal_common_nodes_values_in_columns, (i + 1) * nr_equal_common_nodes_values_in_columns] for i in
                        xrange(nr_common_nodes_values_combinations)]
         return np.array([self.probs[bounds[0] : bounds[1], -1] for bounds in bounds_list]).astype(np.float)
 
-    ## Finished
-def get_probs(factor1, factor2, products, nr_common_nodes, nodes):
-    values1 = factor1.probs[:, :-1] # Includes common_nodes columns
-    values2 = factor2.probs[:, nr_common_nodes:-1] # Excludes common_nodes columns
+    def normalize(self):
+        prob_column = self.probs[:,-1].astype(np.float).reshape((-1, 1))
+        prob_column = prob_column / np.sum(prob_column)
 
-    if values1[:, nr_common_nodes:].size != 0 and values2.size != 0:
-        nr_rows = np.prod([len(factor1.network.values[node] for node in nodes)])
+        probs = np.hstack((self.probs[:,:-1].reshape((-1, self.nr_nodes)), prob_column))
 
-        probs = [np.repeat(values1, nr_rows / len(values1), axis=0)]
-        probs += [np.tile(values2.T, nr_rows / len(values2)).T]
-    elif values1[:, nr_common_nodes:].size != 0:
-        probs = [values1]
-    elif values2.size != 0:
-        probs = [np.repeat(values1, len(values2) / len(values1), axis=0)]
-        probs += [values2]
-    else:
-        probs = [values1]
-    probs += [products]
-
-    return np.hstack(probs)
+        return Factor(self.nodes, probs, self.network)
