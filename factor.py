447d4352683049fa95397518c2ea599c6f27da16
ovacia345 <nielsvann@hotmail.nl>
2018-05-20 22:22:48 +0200

    Add comments and improve readability

--- factor.py
+++ factor.py
@@ -10,7 +10,7 @@ class Factor():
 
         self.network = network
 
-
+    # This function computes the product of all the factors in the factors parameter.
     @staticmethod
     def product(factors):
         for i in xrange(factors.size - 1):
@@ -18,17 +18,19 @@ class Factor():
             factors = np.delete(factors, 0)
         return factors[0]
 
+    # When doing factor multiplication, the resulting factor might have more nodes.
+    # This function returns the probs of such a bigger factor.
     @staticmethod
     def get_probs(factor1, factor2, prob_column, nr_common_nodes):
-        values1 = factor1.probs[:, :-1]  # Includes common_nodes columns
-        values2 = factor2.probs[:, nr_common_nodes:-1]  # Excludes common_nodes columns
+        values1 = factor1.probs[:, :-1]  # Includes common nodes columns
+        values2 = factor2.probs[:, nr_common_nodes:-1]  # Excludes common nodes columns
 
-        if values1[:, nr_common_nodes:].size > 0 and values2.size > 0:
+        if factor1.nr_nodes > nr_common_nodes and factor2.nr_nodes > nr_common_nodes:
             nr_rows = prob_column.size
 
             probs = [np.repeat(values1, nr_rows / values1.shape[0], axis=0)]
             probs += [np.tile(values2.T, nr_rows / values2.shape[0]).T]
-        elif values2.size > 0:
+        elif factor2.nr_nodes > nr_common_nodes:
             probs = [np.repeat(values1, values2.shape[0] / values1.shape[0], axis=0)]
             probs += [values2]
         else:
@@ -38,31 +40,35 @@ class Factor():
         return np.hstack(probs)
 
 
+    # This function reduces a factor given the nodes that are in the factor that are observed
     def reduce(self, nodes_in_observed, observed):
         self.sort(nodes_in_observed)
+
+        nr_nodes_in_observed = nodes_in_observed.size
+        nodes = self.nodes[nr_nodes_in_observed:]
 
         nodes_in_observed = [(node, observed[node]) for node in nodes_in_observed]
         nodes_in_observed_values_data = [(self.network.values[node].index(value), len(self.network.values[node]))
                                          for node, value in nodes_in_observed]
+                                         # List of tuples (index of value in values, number of values)
 
         nr_rows = self.probs.shape[0]
-        nr_rows_list = [nr_rows]*len(nodes_in_observed)
-        for i, (_, nr_values) in enumerate(nodes_in_observed_values_data):
-            nr_rows_list[i:] = [nr_rows_list[i] / nr_values]*len(nr_rows_list[i:])
+        bounds = [0, nr_rows]
+        for index, nr_values in nodes_in_observed_values_data:
+            nr_rows /= nr_values
+            bounds[0] += index * nr_rows
+            bounds[1] -= (nr_values - index - 1) * nr_rows
 
-        bounds = [np.sum([index * nr_rows_list[i] for i, (index, _) in enumerate(nodes_in_observed_values_data)])]
-        bounds += [nr_rows - np.sum([(nr_values - (index + 1)) * nr_rows_list[i] for i, (index, nr_values) in enumerate(nodes_in_observed_values_data)])]
-
-        probs = self.probs[bounds[0]:bounds[1], len(nodes_in_observed):]
-
-        nodes = self.nodes[len(nodes_in_observed):]
+        probs = self.probs[bounds[0]:bounds[1], nr_nodes_in_observed:]
 
         return Factor(nodes, probs, self.network)
 
+    # This function computes the multiplication of this factor with another factor
     def times(self, factor):
         common_nodes = np.intersect1d(self.nodes, factor.nodes)
         nr_common_nodes = common_nodes.size
-        nr_common_nodes_values_combinations = np.prod([len(self.network.values[common_node]) for common_node in common_nodes])
+        nr_common_nodes_values_combinations = np.prod([len(self.network.values[common_node])
+                                                       for common_node in common_nodes])
 
         self.sort(common_nodes)
         factor.sort(common_nodes)
@@ -71,7 +77,8 @@ class Factor():
 
         products1 = self.products_with_equal_common_nodes(nr_common_nodes_values_combinations)
         products2 = factor.products_with_equal_common_nodes(nr_common_nodes_values_combinations)
-        products_list = [[products1[i, e] * products2[i] for e in xrange(products1.shape[1])] for i in xrange(products1.shape[0])]
+        products_list = [[products1[i, e] * products2[i] for e in xrange(products1.shape[1])]
+                         for i in xrange(products1.shape[0])]
         products_columns = [product.reshape((-1, 1)) for products in products_list for product in products]
         products = np.vstack(products_columns)
 
@@ -84,12 +91,13 @@ class Factor():
         nr_values = len(self.network.values[node])
         nr_equal_values_in_column = self.probs.shape[0] / nr_values
 
-        indices_list = [[i * nr_equal_values_in_column + e for i in xrange(nr_values)] for e in xrange(nr_equal_values_in_column)]
+        values = self.probs[:nr_equal_values_in_column, 1:-1]
+
+        indices_list = [[i * nr_equal_values_in_column + e for i in xrange(nr_values)]
+                        for e in xrange(nr_equal_values_in_column)]
         sums_list = [self.probs[indices, -1] for indices in indices_list]
         sums = np.array(sums_list).astype(np.float)
         sums = np.sum(sums, axis = 1).reshape((-1, 1))
-
-        values = self.probs[:nr_equal_values_in_column, 1:-1]
 
         probs = np.hstack((values, sums))
 
@@ -98,14 +106,18 @@ class Factor():
     def normalize(self, query):
         self.sort(query)
 
+        values = self.probs[:, :-1].reshape((-1, self.nr_nodes))
+
         prob_column = self.probs[:,-1].astype(np.float).reshape((-1, 1))
         prob_column = prob_column / np.sum(prob_column)
 
-        probs = np.hstack((self.probs[:,:-1].reshape((-1, self.nr_nodes)), prob_column))
+        probs = np.hstack((values, prob_column))
 
         return Factor(self.nodes, probs, self.network)
 
 
+    # All functions that are used in variable_elim.py do not change the factor.
+    # This function does change the factor, because it is only used in factor.py.
     def sort(self, nodes):
         for i, node in enumerate(nodes):
             index = np.flatnonzero(self.nodes == node)[0]
@@ -120,9 +132,10 @@ class Factor():
 
         self.probs = np.array(map(list, self.probs))
 
+    # This function returns the parts that will be multiplied in the times function.
     def products_with_equal_common_nodes(self, nr_common_nodes_values_combinations):
-        nr_equal_common_nodes_values_in_columns = self.probs.shape[0] / nr_common_nodes_values_combinations
-        bounds_list = [[i * nr_equal_common_nodes_values_in_columns, (i + 1) * nr_equal_common_nodes_values_in_columns] for i in
-                       xrange(nr_common_nodes_values_combinations)]
+        nr_equal_common_nodes_values = self.probs.shape[0] / nr_common_nodes_values_combinations
+        bounds_list = [[i * nr_equal_common_nodes_values, (i + 1) * nr_equal_common_nodes_values]
+                       for i in xrange(nr_common_nodes_values_combinations)]
         products = [self.probs[bounds[0]:bounds[1], -1] for bounds in bounds_list]
         return np.array(products).astype(np.float)
