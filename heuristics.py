5d80391a8fb2396c0e2342b0b869b4fa0a160fc5
ovacia345 <nielsvann@hotmail.nl>
2018-05-23 20:55:27 +0200

    Add min-neighbours heuristic

--- heuristics.py
+++ heuristics.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-def chooseHeuristic(network, evidence, query, heuristic):
-    if heuristic == 'least-incoming-arcs first':
-        return least_incoming_arcs_first(network, evidence, query)
+def min_neighbours(network, evidence, query):
+    neighbours = {node: set() for node in network.nodes}
+    for node in network.nodes:
+        neighbours[node].update(network.parents[node])
+        for parent in network.parents[node]:
+            neighbours[parent].add(node)
 
-def least_incoming_arcs_first(network, evidence, query):
-    nodes = np.array(network.nodes)
-    nodes = nodes[np.in1d(nodes, evidence.keys(), invert = True)]
-    nodes = nodes[nodes != query]
-
-    nr_incoming_arcs = [len(network.parents[node]) for node in nodes]
-    sorted_indices = np.argsort(nr_incoming_arcs)
-
-    return nodes[sorted_indices]
+    for key in evidence.keys():
+        neighbours.pop(key)
+    neighbours.pop(query)
+    nr_neighbours = [len(neighbours[node]) for node in neighbours.keys()]
+    indices = np.argsort(nr_neighbours)
+    elim_order = np.array(neighbours.keys())[indices]
+    return elim_order
